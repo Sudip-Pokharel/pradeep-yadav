172dec6918122b8ee488c31080332601b15b5219
Sudip Pokharel <sudieep661@gmail.com>
2025-07-19 18:17:04 +0545

    website initial design - includes desktop design - static data - only added in html - mobile responsive rem.

---
+++ main.js
@@ -0,0 +1,21 @@
+new Swiper('.swiper-service', {
+    loop: true,
+    autoplay: { delay: 3000 },
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+    nextEl: '#custom-service-next',
+    prevEl: '#custom-service-prev',
+  },
+});
+
+new Swiper('.swiper-international-relation', {
+    loop: true,
+    autoplay: { delay: 3000 },
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+    nextEl: '#custom-internal-relation-next',
+    prevEl: '#custom-internal-relation-prev',
+  },
+});
